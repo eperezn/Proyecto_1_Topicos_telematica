acd63d8bfee71f4685c2fbe1bdcf546ccf6fe50e
eperezn <32418364+eperezn@users.noreply.github.com>
2019-02-03 15:39:04 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,28 @@
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost:27017/proyecto1';
-mongoose.connect(mongoDB,  { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.once('open', function() {
-    console.log("Connected to DB")
-    });
-module.exports = mongoose;
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'test';
+
+var config = {
+  development: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'articulos'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/proyecto1'
+  },
+
+  test: {
+//    baseUrl: "/nodeArticulos/",
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'articulos'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo-server/proyecto1'
+  }
+};
+
+module.exports = config[env];
